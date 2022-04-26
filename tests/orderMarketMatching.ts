67dff3ce331fedaacf7d410e0265c90ca86eea44
Mamchuk1 <mamchuk70@gmail.com>
2022-04-26 13:15:37 +0700

    buy balance fix

--- tests/orderMarketMatching.ts
+++ tests/orderMarketMatching.ts
@@ -11,3 +11,24 @@ let awaity = require('./datas/awaity');
 let enterAdm = require('./enter_adm');
 let enterP = require('./enter');
 
+(async function matching() {
+    const driver = new Builder().forBrowser('chrome').build();
+    const driverTwo = new Builder().forBrowser('chrome').build();
+
+    await enterP.enterThree(driver);
+    console.log('enter success');
+    // Взятие балансов
+    await driver.findElement(By.className("SELECTOR-CHECK-PAIR-pzm_tdel")).click();
+    let pzm_tdel_balanceB = await selectors.balanceBuy(driver).getText();
+    console.log('balance buy = ', pzm_tdel_balanceB);
+    let pzm_tdel_balanceS = await selectors.balanceSell(driver).getText();
+    console.log('balance sell = ', pzm_tdel_balanceS);
+
+    // Приведение баланса Buy в число
+    pzm_tdel_balanceB = pzm_tdel_balanceB.slice(0, -4);
+    console.log(pzm_tdel_balanceB);
+    let pzm_tdel_balanceBstr = pzm_tdel_balanceB.replace(/[\s,]/g, '');
+    console.log(pzm_tdel_balanceBstr);
+    let pzm_tdel_balanceBnum:number = +pzm_tdel_balanceBstr;
+
+})
